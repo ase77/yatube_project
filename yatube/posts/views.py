a4896de45a71906b07b59a08d991bfee826443d6
Антон Моторин <ust.mir@gmail.com>
2021-12-20 15:40:09 +0300

    Добавлены шаблоны, миграции

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,21 @@
-#from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, template, context)
 
-def group_posts(request):
-    return HttpResponse('Страницы сообществ')
-
-def group_posts_detail(request, slug):
-    return HttpResponse(f'Страница сообщества {slug}')
+def group_posts(request, slug):
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:2]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
